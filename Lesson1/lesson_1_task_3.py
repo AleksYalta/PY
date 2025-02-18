d89fcf9df31cddebe0cfb321ca24cb4dcde2eda4
AleksYalta <kartaguzov.aleks@gmail.com>
2025-02-18 12:11:53 +0300

    Lesson1

--- Lesson1/lesson_1_task_3.py
+++ Lesson1/lesson_1_task_3.py
@@ -1,16 +1,3 @@
-first_name = "Aleksandr"
-last_name = "Kartaguzov"
-print(f"Вас зовут: {first_name} {last_name}")
-
-first_name = "Aleksandr"
-last_name = "Kartaguzov"
-print("Вас зовут: " + first_name + " " + last_name)
-
-first_name = input("Введите ваше имя:")
-last_name = input("Введите фамилию: ")
-print("Вас зовут: " + first_name + " " + last_name)
-
 first_name = input("Введите ваше имя:")
 last_name = input("Введите вашу фамилию: ")
-
 print(f"Вас зовут: {last_name} {first_name}")
