f090a77e3c553a5f13ee270655040ca2680a16dd
AleksYalta <kartaguzov.aleks@gmail.com>
2025-04-17 16:29:42 +0300

    Lesson8

---
+++ 08_lesson/test_post_positive.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def test_create_project_positive(base_url, headers):
+    payload = {
+        "title": "ГосУслуги",
+        "users": {
+            "10e6c06d-a00b-4e3a-8fee-d57417921ffa": "worker"
+        }
+    }
+
+    response = requests.post(f"{base_url}/api-v2/projects",
+                             headers=headers, json=payload)
+
+    print("Response status code:", response.status_code)
+    print("Response content:", response.text)
+
+    assert response.status_code == 201
+
+    data = response.json()
+    print("Response JSON:", data)
+
+    assert "id" in data, "'id' field is missing in the response"
+    assert isinstance(data["id"], str), "'id' should be a string"
